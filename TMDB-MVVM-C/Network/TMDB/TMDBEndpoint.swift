8a5c9f90350f6ae2f5ba660ff225654b8a7f9ba9
David Lee <mikeportnoy168@gmail.com>
2026-04-13 20:21:44 +0800

    feat: add TMDBEndpoint

--- TMDB-MVVM-C/Network/TMDB/TMDBEndpoint.swift
+++ TMDB-MVVM-C/Network/TMDB/TMDBEndpoint.swift
@@ -10,6 +10,7 @@ import Foundation
 enum TMDBEndpoint: Endpoint {
   case popularMovies(page: Int)
   case movieDetail(id: Int)
+  case searchMovies(query: String, page: Int)
   
   var baseURL: String { "https://api.themoviedb.org/3" }
   
@@ -19,20 +20,49 @@ enum TMDBEndpoint: Endpoint {
       return "/movie/popular"
     case .movieDetail(let id): 
       return "/movie/\(id)"
+    case .searchMovies:
+      return "/search/movie"
     }
   }
   
   var method: HTTPMethod { .get }
   
   var queryItems: [URLQueryItem] {
-    var items = [URLQueryItem(name: "api_key",
-                              value: AppConfiguration.tmdbAPIKey)]
+    var items = [
+      URLQueryItem(
+        name: "api_key",
+        value: AppConfiguration.tmdbAPIKey
+      )
+    ]
+    
     switch self {
     case .popularMovies(let page):
-      items.append(URLQueryItem(name: "page", value: "\(page)"))
+      items.append(
+        URLQueryItem(
+          name: "page",
+          value: "\(page)"
+        )
+      )
+      
     case .movieDetail:
       break
+      
+    case .searchMovies(let query, let page):
+      items.append(
+        URLQueryItem(
+          name: "query",
+          value: query
+        )
+      )
+      
+      items.append(
+        URLQueryItem(
+          name: "page",
+          value: "\(page)"
+        )
+      )
     }
+    
     return items
   }
   
